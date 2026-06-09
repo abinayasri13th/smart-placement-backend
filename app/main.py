0e498166c338c41aa8c4235128585b7984044af5
Abinayasri <abinayasri13th@gmail.com>
2026-06-09 13:11:53 +0530

    fix cors for production

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ app = FastAPI(title="Smart Placement Assistant API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["*"],
+
+   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
